9ec0e3783f8ce214799328ef8e663f170076cfe0
Dyer Lytle <dlytle@lowell.edu>
2020-11-26 09:18:59 -0700

    Updated amqlog.go to use goroutines to run the topic subscriptions while the main routine waits forever. Also, the messages are written to a file along with their date/time. j

--- amqlog.go
+++ amqlog.go
@@ -8,79 +8,51 @@ import (
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-//Connect to ActiveMQ and listen for messages
+func checktopic(s *stomp.Subscription, f *os.File, name string) {
+	for {
+		msg := <-s.C
+		currentTime := time.Now()
+		_, _ = f.WriteString(currentTime.Format("2006.01.02 15:04:05") +
+			name + string(msg.Body) + "\n")
+		fmt.Printf(currentTime.Format("2006.01.02 15:04:05") +
+			"Wrote bytes " + name + "\n")
+	}
+}
+
 func main() {
+	// Connect to ActiveMQ on joe (the computer)
+	// Use HeartBeat to tell the connection to not timeout.
 	conn, err := stomp.Dial("tcp", "joe.lowell.edu:61613",
-		stomp.ConnOpt.HeartBeat(6 * time.Hour, 6 * time.Hour))
+		stomp.ConnOpt.HeartBeat(6*time.Hour, 6*time.Hour))
+	defer conn.Disconnect()
+
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// Create and open a log file on disk.
 	f, err := os.Create("/home/pi/go/amqoutput")
 	check(err)
 	defer f.Close()
 
-	subRC1, err := conn.Subscribe("/topic/LOUI.RC1.loisCommandResult", stomp.AckAuto)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for {
-		msg := <-subRC1.C
-        _, err = f.WriteString("RC1: " + string(msg.Body) + "\n\n")
-		fmt.Printf("Wrote bytes\n")
-		fmt.Println(string(msg.Body))
-	}
-	subRC2, err := conn.Subscribe("/topic/LOUI.RC2.loisCommandResult", stomp.AckAuto)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for {
-		msg := <-subRC2.C
-        _, err = f.WriteString("RC2: " + string(msg.Body) + "\n\n")
-		fmt.Printf("Wrote bytes\n")
-		fmt.Println(string(msg.Body))
-	}
-	subGDR, err := conn.Subscribe("/topic/LOUI.GDR.loisCommandResult", stomp.AckAuto)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for {
-		msg := <-subGDR.C
-        _, err = f.WriteString("GDR: " + string(msg.Body) + "\n\n")
-		fmt.Printf("Wrote bytes\n")
-		fmt.Println(string(msg.Body))
-	}
-	subWFS, err := conn.Subscribe("/topic/LOUI.WFS.loisCommandResult", stomp.AckAuto)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for {
-		msg := <-subWFS.C
-        _, err = f.WriteString("WFS: " + string(msg.Body) + "\n\n")
-		fmt.Printf("Wrote bytes\n")
-		fmt.Println(string(msg.Body))
-	}
+	// Subscribe to various topics.
+	wrs, err := conn.Subscribe("/topic/wrs.loisTelemetry", stomp.AckAuto)
+	defer wrs.Unsubscribe()
+	aos, err := conn.Subscribe("/topic/AOS.AOSPubDataSV.AOSDataPacket",
+		stomp.AckAuto)
+	defer wrs.Unsubscribe()
 
-	err = subRC1.Unsubscribe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = subRC2.Unsubscribe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = subGDR.Unsubscribe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = subWFS.Unsubscribe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer conn.Disconnect()
+	// The listeners for the various subscriptions are handled as goroutines.
+	fmt.Println("starting wrs")
+	go checktopic(wrs, f, "wrs")
+	fmt.Println("starting aos")
+	go checktopic(aos, f, "aos")
+
+	// Tell the main program to wait forever.
+	select {}
 }
